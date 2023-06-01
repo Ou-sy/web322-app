b820a1127c99cfcce9eefd564c8cd3be2dbeb396
Song-Yun Ou <sou12@myseneca.ca>
2023-06-01 11:50:45 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Song-Yun Ou Student ID: 174196212 Date: June 05, 2023
+*
+*  Cyclic Web App URL: ________________________________________________________
+*
+*  GitHub Repository URL: ______________________________________________________
+*
+********************************************************************************/ 
+
