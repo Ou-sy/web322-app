eeb0aa876bf26696009feffe7e8d56d42bd74792
Song-Yun Ou <sou12@myseneca.ca>
2023-06-01 12:38:33 -0400

    part-1 submit

--- server.js
+++ server.js
@@ -10,4 +10,29 @@
 *  GitHub Repository URL: ______________________________________________________
 *
 ********************************************************************************/ 
+const express = require("express")
+const app = express();
+var path = require("path");
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+// setup the static folder that static resources can load from
+// like images, css files, etc.
+app.use(express.static("static"));
+
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+    res.redirect("/about");
+});
+app.get("/about", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/about.html"));
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT, onHttpStart);
+
 
